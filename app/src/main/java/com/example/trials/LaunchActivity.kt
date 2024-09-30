52d725abd4d88fff77902158e4aeb3abc5976ec7
raynald <raynaldgitau@gmail.com>
2024-09-30 23:01:57 +0300

    added post job list

---
+++ app/src/main/java/com/example/trials/LaunchActivity.kt
@@ -0,0 +1,23 @@
+package com.example.trials
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class LaunchActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch2)
+
+        val button = findViewById<Button>(R.id.btnGetstart)
+
+        button.setOnClickListener {
+            val intent = Intent(this,SignInActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/trials/PostJobsList.kt
@@ -0,0 +1,12 @@
+package com.example.trials
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostJobsList :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_postjobs_list)
+
+    }
+}
